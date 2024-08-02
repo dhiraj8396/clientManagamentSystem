9cd33b88cace426d4d1fe44c225b8798cfb61d7f
dhiraj8396 <160827527+dhiraj8396@users.noreply.github.com>
2024-08-02 17:02:42 +0530

    Update models.py

--- ClienManagementSystem/ClientAPP/models.py
+++ ClienManagementSystem/ClientAPP/models.py
@@ -1,23 +1,14 @@
-import email
-
 from django.db import models
 
-# Create your models here.
-from django.db import models
 
-# Create your models here.
-class Client(models.Model):
-    id = models.IntegerField(primary_key=True)
+class ClientModel(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    company= models.CharField(max_length=50)
-    email_id= models.EmailField()
-    department= models.CharField(max_length=50)
-    dateOfJoining=models.DateField()
-    status=models.CharField(max_length=30)
-    projectDomain=models.CharField(max_length=40)
-
-
-
-
-
-
+    company = models.CharField(max_length=50)
+    email_id = models.EmailField(unique=True)
+    department = models.CharField(max_length=50)
+    dateofjoining = models.DateField()
+    STATUS_CHOICES = [('onboarded', 'Onboarded'),('not_onboarded', 'Not Onboarded'),]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    DOMAIN_CHOICES = [('sass', 'SaaS'),('ecommerce', 'E-Commerce'),('crm', 'CRM'),('erp', 'ERP'),('finance', 'Finance'),]
+    projectdomain = models.CharField(max_length=20, choices=DOMAIN_CHOICES)
