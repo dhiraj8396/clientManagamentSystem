f4e5aff775f45e200ca160f90fe214bd3fa5d407
dhiraj8396 <160827527+dhiraj8396@users.noreply.github.com>
2024-08-02 17:00:53 +0530

    Update views.py

--- ClienManagementSystem/ClientAPP/views.py
+++ ClienManagementSystem/ClientAPP/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
-from .models import Client
-# Create your views here.
-def AddNewClientView(request):
-    client_objs =Client.objects.all()
-    template_name = "ClientApp/add.html"
-    context ={"client_objes":client_objs}
+# Assuming you have already imported necessary modules and defined your forms and models
+
+from django.shortcuts import render, redirect
+from .forms import ClientForm
+from .models import ClientModel
+
+def AddClient(request):
+    form = ClientForm()
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('showurl')
+    template_name = 'add.html'
+    context = {'form': form}
     return render(request, template_name, context)
+
+def ShowClient(request):
+    obj = ClientModel.objects.all()
+    template_name = 'show.html'
+    context = {'obj': obj}
+    return render(request, template_name, context)
+
+def UpdateClient(request, id):
+    obj = ClientModel.objects.get (id=id)
+    form = ClientForm(instance=obj)
+    if request.method == 'POST':
+        form = ClientForm(request.POST, instance=obj)
+        if form.is_valid():
+            form.save()
+            return redirect('showurl')
+    template_name = 'add.html'
+    context = {'form': form}
+    return render(request, template_name, context)
+
+def DeleteClient(request, id):
+    obj = ClientModel.objects.get (id=id)
+    obj.delete()
+    return redirect('showurl')
