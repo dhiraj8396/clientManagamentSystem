ee786c0293c262e8eaa93eb998bc0e7e9d88d965
dhiraj8396 <160827527+dhiraj8396@users.noreply.github.com>
2024-08-02 17:01:57 +0530

    Update urls.py

--- ClienManagementSystem/ClientAPP/urls.py
+++ ClienManagementSystem/ClientAPP/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
 
+from django.urls import path
 from . import views
 
-urlpatterns=[
-    path("add/",views.AddNewClientView)
+urlpatterns = [
+    path('add/',views.AddClient,name='addurl'),
+    path('show/',views.ShowClient,name='showurl'),
+    path('update/<int:id>/',views.UpdateClient,name='updateurl'),
+    path('delete/<int:id>/',views.DeleteClient,name='deleteurl'),
 ]
